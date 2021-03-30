6f39ad8a6f6c3cace78c73b0bff407018567f0d9
ArgueraDev <robertoarguera@gmail.com>
2021-03-29 21:35:02 -0600

    ...

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,13 @@
+feather.replace();
 var myCarousel = document.querySelector('#myCarousel')
 var carousel = new bootstrap.Carousel(myCarousel, {
   interval: 1000,
   wrap: false
+});
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
 })
